d03704e33cce75a7b9bd18bfc85c75b3a6ba067f
ryan m <contact@ryanmeek.com>
2020-02-28 14:53:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure() {enable :sessions; set :session_secret, "secret"}
   get('/') {erb :index}
-  post('/checkout') {session[:item] = params[:item]; erb :checkout}
+  post('/checkout') {@session[:item] = params[:item]; erb :checkout}
 end
